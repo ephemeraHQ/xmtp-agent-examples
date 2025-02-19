d9982061a933fe9fb90f271fba92969c9c675b29
fabri <fguespe@gmail.com>
2025-02-19 14:34:04 -0300

    fix gen keys

--- scripts/generateKeys.ts
+++ scripts/generateKeys.ts
@@ -1,17 +1,14 @@
 import { writeFile } from "node:fs/promises";
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
+import { join } from "node:path";
 import { generatePrivateKey } from "viem/accounts";
 import { generateEncryptionKeyHex } from "@/helpers";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 console.log("Generating keys...");
 
 const walletKey = generatePrivateKey();
 const encryptionKeyHex = generateEncryptionKeyHex();
 
-const filePath = join(".", ".env");
+const filePath = join(process.cwd(), ".env");
 
 await writeFile(
   filePath,
